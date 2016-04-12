dbf60407a4c9e1b1d6df4652585b100fdbafdd48
musyoku <musyoku@hellowork.jp>
2016-04-12 14:23:44 +0900

    j

---
+++ util.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import os, re, math, pylab
+from math import *
+import numpy as np
+from StringIO import StringIO
+from PIL import Image
+from chainer import cuda, Variable, function
+from chainer.utils import type_check
+
+def load_images(args, convert_to_grayscale=True):
+	dataset = []
+	fs = os.listdir(args.image_dir)
+	print "loading", len(fs), "images..."
+	for fn in fs:
+		f = open("%s/%s" % (args.image_dir, fn), "rb")
+		if convert_to_grayscale:
+			img = np.asarray(Image.open(StringIO(f.read())).convert("L"), dtype=np.float32) / 255.0
+		else:
+			img = np.asarray(Image.open(StringIO(f.read())).convert("RGB"), dtype=np.float32).transpose(2, 0, 1) / 255.0
+		img = (img - 0.5) / 0.5
+		dataset.append(img)
+		f.close()
+	return dataset
+
+def load_labeled_dataset(args, convert_to_grayscale=True):
+	dataset = []
+	labels = []
+	fs = os.listdir(args.image_dir)
+	print "loading", len(fs), "images..."
+	for fn in fs:
+		m = re.match("(.)_.+", fn)
+		label = int(m.group(1))
+		f = open("%s/%s" % (args.image_dir, fn), "rb")
+		if convert_to_grayscale:
+			img = np.asarray(Image.open(StringIO(f.read())).convert("L"), dtype=np.float32) / 255.0
+		else:
+			img = np.asarray(Image.open(StringIO(f.read())).convert("RGB"), dtype=np.float32).transpose(2, 0, 1) / 255.0
+		img = (img - 0.5) / 0.5
+		dataset.append(img)
+		labels.append(label)
+		f.close()
+	return dataset, labels
+
+class Adder(function.Function):
+	def check_type_forward(self, in_types):
+		n_in = in_types.size()
+		type_check.expect(n_in == 2)
+		x_type, label_type = in_types
+
+		type_check.expect(
+			x_type.dtype == np.float32,
+			label_type.dtype == np.float32,
+			x_type.ndim == 2,
+			label_type.ndim == 2,
+		)
+
+	def forward(self, inputs):
+		xp = cuda.get_array_module(inputs[0])
+		x, label = inputs
+		n_batch = x.shape[0]
+		output = xp.empty((n_batch, x.shape[1] + label.shape[1]), dtype=xp.float32)
+		output[:,:x.shape[1]] = x
+		output[:,x.shape[1]:] = label
+		return output,
+
+	def backward(self, inputs, grad_outputs):
+		x, label = inputs
+		return grad_outputs[0][:,:x.shape[1]], grad_outputs[0][:,x.shape[1]:]
+
+def add_label(x, label):
+	return Adder()(x, label)
---
+++ vae.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+import numpy as np
+import chainer
+from chainer import cuda, Variable, function, FunctionSet, optimizers
+from chainer import functions as F
+from chainer import links as L
+from activations import activations
+
+class VAE():
+	def __init__(self, encoder, decoder):
+		self.encoder = encoder
+		self.decoder = decoder
+
+	def encode(self, x):
+		return self.encoder(x)
+
+	def decode(self, x):
+		return self.decoder(x)
+
+	def loss(self, x):
+		pass
+
+class Encoder(chainer.Chain):
+	def __init__(self, **layers):
+		super(Encoder, self).__init__(**layers)
+
+	def forward_one_step(self, x, test):
+		activate = activations[self.activation_type]
+
+		chain_mean = [x]
+		chain_variance = [x]
+
+		# Hidden
+		for i in range(self.n_layers - 1):
+			u = getattr(self, "layer_mean_%i" % i)(chain_mean[-1])
+			if self.apply_batchnorm:
+				u = getattr(self, "batchnorm_mean_%i" % i)(u, test=test)
+			output = activate(u)
+			if self.enable_dropout:
+				output = F.dropout(output, train=not test)
+			chain_mean.append(output)
+
+			u = getattr(self, "layer_variance_%i" % i)(chain_variance[-1])
+			if self.apply_batchnorm:
+				u = getattr(self, "batchnorm_variance_%i" % i)(u, test=test)
+			output = activate(u)
+			if self.enable_dropout:
+				output = F.dropout(output, train=not test)
+			chain_variance.append(output)
+
+		# Output
+		u = getattr(self, "layer_mean_%i" % (self.n_layers - 1))(chain_mean[-1])
+		if self.apply_batchnorm_to_output:
+			u = getattr(self, "batchnorm_mean_%i" % (self.n_layers - 1))(u, test=test)
+		if self.output_activation_type is None:
+			chain_mean.append(u)
+		else:
+			chain_mean.append(activations[self.output_activation_type](u))
+
+		u = getattr(self, "layer_variance_%i" % (self.n_layers - 1))(chain_variance[-1])
+		if self.apply_batchnorm_to_output:
+			u = getattr(self, "batchnorm_variance_%i" % (self.n_layers - 1))(u, test=test)
+		if self.output_activation_type is None:
+			chain_variance.append(u)
+		else:
+			chain_variance.append(activations[self.output_activation_type](u))
+
+		mean = chain_mean[-1]
+
+		# log(sigma^2)
+		ln_var = chain_variance[-1]
+
+		return F.gaussian(mean, ln_var)
+
+class BernoulliDecder(chainer.Chain):
+	def __init__(self, **layers):
+		super(Decder, self).__init__(**layers)
+
+	def forward_one_step_deteministic(self, x, test):
+		activate = activations[self.activation_type]
+		chain = [x]
+
+		# Hidden
+		for i in range(self.n_layers - 1):
+			u = getattr(self, "layer_%i" % i)(chain[-1])
+			if i == 0:
+				if self.apply_batchnorm_to_input:
+					u = getattr(self, "batchnorm_%i" % i)(u, test=test)
+			else:
+				if self.apply_batchnorm:
+					u = getattr(self, "batchnorm_%i" % i)(u, test=test)
+			output = activate(u)
+			if self.enable_dropout:
+				output = F.dropout(output, train=not test)
+			chain.append(output)
+
+		# Output
+		u = getattr(self, "layer_%i" % (self.n_layers - 1))(chain[-1])
+		if self.apply_batchnorm_to_output:
+			u = getattr(self, "batchnorm_%i" % (self.n_layers - 1))(u, test=test)
+		if self.output_activation_type is None:
+			chain.append(u)
+		else:
+			chain.append(activations[self.output_activation_type](u))
+
+		return chain[-1]
+
+class GaussianDecder(chainer.Chain):
+	def __init__(self, **layers):
+		super(Decder, self).__init__(**layers)
+
+	def forward_one_step(self, x, test):
+		activate = activations[self.activation_type]
+
+		chain_mean = [x]
+		chain_variance = [x]
+
+		# Hidden
+		for i in range(self.n_layers - 1):
+			u = getattr(self, "layer_mean_%i" % i)(chain_mean[-1])
+			if i == 0:
+				if self.apply_batchnorm_to_input:
+					u = getattr(self, "batchnorm_mean_%i" % i)(u, test=test)
+			else:
+				if self.apply_batchnorm:
+					u = getattr(self, "batchnorm_mean_%i" % i)(u, test=test)
+			output = activate(u)
+			if self.enable_dropout:
+				output = F.dropout(output, train=not test)
+			chain_mean.append(output)
+
+			u = getattr(self, "layer_variance_%i" % i)(chain_variance[-1])
+			if i == 0:
+				if self.apply_batchnorm_to_input:
+					u = getattr(self, "batchnorm_variance_%i" % i)(u, test=test)
+			else:
+				if self.apply_batchnorm:
+					u = getattr(self, "batchnorm_variance_%i" % i)(u, test=test)
+			output = activate(u)
+			if self.enable_dropout:
+				output = F.dropout(output, train=not test)
+			chain_variance.append(output)
+
+		# Output
+		u = getattr(self, "layer_mean_%i" % (self.n_layers - 1))(chain_mean[-1])
+		if self.apply_batchnorm_to_output:
+			u = getattr(self, "batchnorm_mean_%i" % (self.n_layers - 1))(u, test=test)
+		if self.output_activation_type is None:
+			chain_mean.append(u)
+		else:
+			chain_mean.append(activations[self.output_activation_type](u))
+
+		u = getattr(self, "layer_variance_%i" % (self.n_layers - 1))(chain_variance[-1])
+		if self.apply_batchnorm_to_output:
+			u = getattr(self, "batchnorm_variance_%i" % (self.n_layers - 1))(u, test=test)
+		if self.output_activation_type is None:
+			chain_variance.append(u)
+		else:
+			chain_variance.append(activations[self.output_activation_type](u))
+
+		mean = chain_mean[-1]
+
+		# log(sigma^2)
+		ln_var = chain_variance[-1]
+
+		return F.gaussian(mean, ln_var)
