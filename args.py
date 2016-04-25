6045785782269470d3a513b6ee2af41843647066
musyoku <musyoku@users.noreply.github.com>
2016-04-25 23:32:55 +0900

    j

--- args.py
+++ args.py
@@ -3,8 +3,8 @@ import argparse
 
 # Arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--image_dir", type=str, default="../images")
+parser.add_argument("--use_gpu", type=bool, default=True)
+parser.add_argument("--image_dir", type=str, default="images")
 parser.add_argument("--model_dir", type=str, default="model")
-parser.add_argument("--visualization_dir", type=str, default="visualization")
-parser.add_argument("--load_epoch", type=int, default=0)
+parser.add_argument("--vis_dir", type=str, default="visualization")
 args = parser.parse_args()
--- vae.py
+++ vae.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
-import time
 import numpy as np
 import chainer
 from chainer import cuda, Variable, function, FunctionSet, optimizers
@@ -25,6 +23,13 @@ class VAE():
 class Encoder(chainer.Chain):
 	def __init__(self, **layers):
 		super(Encoder, self).__init__(**layers)
+		self.apply_batchnorm_to_input = True
+		self.apply_batchnorm = True
+		self.apply_dropout = True
+
+	@property
+	def xp(self):
+		return np if self.layer_0._cpu else cuda.cupy
 
 	def forward_one_step(self, x, test):
 		activate = activations[self.activation_type]
@@ -33,46 +38,42 @@ class Encoder(chainer.Chain):
 		chain_variance = [x]
 
 		# Hidden
-		for i in range(self.n_layers - 1):
-			u = getattr(self, "layer_mean_%i" % i)(chain_mean[-1])
-			if self.apply_batchnorm:
-				u = getattr(self, "batchnorm_mean_%i" % i)(u, test=test)
+		for i in range(self.n_layers):
+			u = chain_mean[-1]
+			if i == 0:
+				if self.apply_batchnorm_to_input:
+					u = getattr(self, "batchnorm_mean_%d" % i)(u, test=test)
+			else:
+				if self.apply_batchnorm:	
+					u = getattr(self, "batchnorm_mean_%d" % i)(u, test=test)
 			output = activate(u)
-			if self.enable_dropout:
+			if self.apply_dropout:
 				output = F.dropout(output, train=not test)
 			chain_mean.append(output)
 
-			u = getattr(self, "layer_variance_%i" % i)(chain_variance[-1])
-			if self.apply_batchnorm:
-				u = getattr(self, "batchnorm_variance_%i" % i)(u, test=test)
+			u = chain_variance[-1]
+			if i == 0:
+				if self.apply_batchnorm_to_input:
+					u = getattr(self, "batchnorm_variance_%i" % i)(u, test=test)
+			else:
+				if self.apply_batchnorm:
+					u = getattr(self, "batchnorm_variance_%i" % i)(u, test=test)
 			output = activate(u)
-			if self.enable_dropout:
+			if self.apply_dropout:
 				output = F.dropout(output, train=not test)
 			chain_variance.append(output)
-
-		# Output
-		u = getattr(self, "layer_mean_%i" % (self.n_layers - 1))(chain_mean[-1])
-		if self.apply_batchnorm_to_output:
-			u = getattr(self, "batchnorm_mean_%i" % (self.n_layers - 1))(u, test=test)
-		if self.output_activation_type is None:
-			chain_mean.append(u)
-		else:
-			chain_mean.append(activations[self.output_activation_type](u))
-
-		u = getattr(self, "layer_variance_%i" % (self.n_layers - 1))(chain_variance[-1])
-		if self.apply_batchnorm_to_output:
-			u = getattr(self, "batchnorm_variance_%i" % (self.n_layers - 1))(u, test=test)
-		if self.output_activation_type is None:
-			chain_variance.append(u)
-		else:
-			chain_variance.append(activations[self.output_activation_type](u))
 
 		mean = chain_mean[-1]
 
 		# log(sigma^2)
 		ln_var = chain_variance[-1]
 
-		return F.gaussian(mean, ln_var)
+		if hasattr(F, "gaussian"):
+			return F.gaussian(mean, ln_var)
+
+		# For old chaier
+		eps = Variable(self.xp.random.normal(0, 1, (x.data.shape[0], ln_var.data.shape[1])).astype("float32"))
+		return mean + F.exp(ln_var) * eps
 
 class BernoulliDecder(chainer.Chain):
 	def __init__(self, **layers):
@@ -92,7 +93,7 @@ class BernoulliDecder(chainer.Chain):
 				if self.apply_batchnorm:
 					u = getattr(self, "batchnorm_%i" % i)(u, test=test)
 			output = activate(u)
-			if self.enable_dropout:
+			if self.apply_dropout:
 				output = F.dropout(output, train=not test)
 			chain.append(output)
 
@@ -127,7 +128,7 @@ class GaussianDecder(chainer.Chain):
 				if self.apply_batchnorm:
 					u = getattr(self, "batchnorm_mean_%i" % i)(u, test=test)
 			output = activate(u)
-			if self.enable_dropout:
+			if self.apply_dropout:
 				output = F.dropout(output, train=not test)
 			chain_mean.append(output)
 
@@ -139,7 +140,7 @@ class GaussianDecder(chainer.Chain):
 				if self.apply_batchnorm:
 					u = getattr(self, "batchnorm_variance_%i" % i)(u, test=test)
 			output = activate(u)
-			if self.enable_dropout:
+			if self.apply_dropout:
 				output = F.dropout(output, train=not test)
 			chain_variance.append(output)
 
