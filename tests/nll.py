bfeaaae136ef23f6aeba72c64fe82adecc3147d3
musyoku <musyoku@users.noreply.github.com>
2016-05-22 01:14:48 +0900

    j

--- tests/nll.py
+++ tests/nll.py
@@ -3,53 +3,74 @@ import sys, os, math
 import numpy as np
 from chainer import Variable
 from chainer import functions as F
+from sklearn import preprocessing
 
 batchsize = 10
 ndim_x = 10
 
 # gaussian
+
+def gaussian_nll_keepbatch(x, mean, ln_var):
+	D = x.data.size
+	x_prec = F.exp(-ln_var)
+	x_diff = x - mean
+	x_power = (x_diff * x_diff) * x_prec * -0.5
+	return (F.sum(ln_var, axis=1) + x.data.shape[1] * math.log(2 * math.pi)) / 2 - F.sum(x_power, axis=1)
+
 print "gaussian:"
 x_batch = np.random.uniform(-10, 10, (batchsize, ndim_x)).astype(np.float32)
 x_batch = Variable(x_batch)
-mean = np.random.uniform(-1, 1, (batchsize, ndim_x)).astype(np.float32)
-ln_var = np.random.uniform(-10, 10, (batchsize, ndim_x)).astype(np.float32)
-mean = Variable(mean)
-ln_var = Variable(ln_var)
-nll = F.gaussian_nll(x_batch, mean, ln_var)
-print nll.data
-nll = F.exp(-nll)
-print nll.data
+mean_batch = np.random.uniform(-1, 1, (batchsize, ndim_x)).astype(np.float32)
+ln_var_batch = np.random.uniform(-10, 10, (batchsize, ndim_x)).astype(np.float32)
+mean_batch = Variable(mean_batch)
+ln_var_batch = Variable(ln_var_batch)
+print "chainer::"
+for i in xrange(batchsize):
+	x = Variable(x_batch.data[i].reshape(1, -1))
+	mean = Variable(mean_batch.data[i].reshape(1, -1))
+	ln_var = Variable(ln_var_batch.data[i].reshape(1, -1))
+	nll = F.gaussian_nll(x, mean, ln_var)
+	print nll.data
 
-# a = 1 / math.sqrt(2 * math.pi * variance) * math.exp(-(x - mu) ** 2 / (2 * variance))
-# print -math.log(a + 1e-6)
-# print math.exp(math.log(a + 1e-6))
+print "keepbatch::"
+nll = gaussian_nll_keepbatch(x_batch, mean_batch, ln_var_batch)
+print nll.data
 
 # kl divergence
+def gaussian_kl_divergence(mean, ln_var):
+	var = F.exp(ln_var)
+	kld = (F.sum(mean * mean, axis=1) + F.sum(var, axis=1) - F.sum(ln_var, axis=1) - mean.data.shape[1]) * 0.5
+	return kld
+
 print "kl divergence"
+print "chainer::"
+for i in xrange(batchsize):
+	mean = Variable(mean_batch.data[i].reshape(1, -1))
+	ln_var = Variable(ln_var_batch.data[i].reshape(1, -1))
+	kld = F.gaussian_kl_divergence(mean, ln_var)
+	print kld.data
 
-J = mean.data.size
-print J
-var = F.exp(ln_var)
-kld = (F.sum(mean * mean) + F.sum(var) - F.sum(ln_var) - J) * 0.5
+print "keepbatch::"
+kld = gaussian_kl_divergence(mean_batch, ln_var_batch)
 print kld.data
-
-kld = F.sum(mean * mean + var - ln_var - mean.data.shape[0], axis=1) * 0.5
-print kld.data
-
-
 
 # bernoulli
-print "bernoulli:"
-x_batch = np.random.uniform(0, 1, (batchsize, ndim_x)).astype(np.float32)
-x_batch = Variable(x_batch)
-y_batch = np.random.uniform(-10, 10, (batchsize, ndim_x)).astype(np.float32)
-y_batch = Variable(y_batch)
-nll = F.bernoulli_nll(x_batch, y_batch)
-print nll.data
+def bernoulli_nll_keepbatch(x, y):
+	nll = F.softplus(y) - x * y
+	return F.sum(nll, axis=1)
 
-p_batch = F.sigmoid(y_batch)
-nll = -(x_batch * F.log(p_batch) + (1 - x_batch) * F.log(1 - p_batch))
-print np.sum(nll.data)
-nll = F.softplus(y_batch) - x_batch * y_batch
-print F.sum(nll, axis=1).data
-print np.sum(nll.data)
+print "bernoulli:"
+x_batch = preprocessing.binarize(np.random.uniform(0, 1, (batchsize, ndim_x)).astype(np.float32), threshold=0.5)
+x_batch = Variable(x_batch)
+y_batch = np.random.uniform(0, 1, (batchsize, ndim_x)).astype(np.float32)
+y_batch = Variable(y_batch)
+print "chainer::"
+for i in xrange(batchsize):
+	x = Variable(x_batch.data[i].reshape(1, -1))
+	y = Variable(y_batch.data[i].reshape(1, -1))
+	nll = F.bernoulli_nll(x, y)
+	print nll.data
+
+print "keepbatch::"
+nll = bernoulli_nll_keepbatch(x_batch, y_batch)
+print nll.data
