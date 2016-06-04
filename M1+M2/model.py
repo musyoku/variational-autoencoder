9e99128d33c9491e5627d96255c4f07a51cd9e77
musyoku <musyoku@users.noreply.github.com>
2016-06-04 16:16:51 +0900

    j

--- M1+M2/model.py
+++ M1+M2/model.py
@@ -8,12 +8,6 @@ conf1 = Conf1()
 conf1.gpu_enabled = True if args.gpu_enabled == 1 else False
 conf1.ndim_x = 28 * 28
 conf1.ndim_z = 50
-conf1.encoder_apply_dropout = True
-conf1.decoder_apply_dropout = True
-conf1.encoder_apply_batchnorm = True
-conf1.encoder_apply_batchnorm_to_input = True
-conf1.decoder_apply_batchnorm = True
-conf1.decoder_apply_batchnorm_to_input = True
 conf1.encoder_hidden_units = [600, 600]
 conf1.decoder_hidden_units = [600, 600]
 vae1 = BernoulliM1VAE(conf1, name="m1")
@@ -24,17 +18,8 @@ conf2 = Conf2()
 conf2.gpu_enabled = True if args.gpu_enabled == 1 else False
 conf2.ndim_x = 50
 conf2.ndim_z = 50
-conf2.encoder_xy_z_apply_dropout = True
-conf2.encoder_x_y_apply_dropout = True
-conf2.decoder_apply_dropout = True
 conf2.encoder_xy_z_hidden_units = [500]
 conf2.encoder_x_y_hidden_units = [500]
 conf2.decoder_hidden_units = [500]
-conf2.encoder_xy_z_apply_batchnorm = True
-conf2.encoder_xy_z_apply_batchnorm_to_input = True
-conf2.encoder_x_y_apply_batchnorm = True
-conf2.encoder_x_y_apply_batchnorm_to_input = True
-conf2.decoder_apply_batchnorm = True
-conf2.decoder_apply_batchnorm_to_input = True
 vae2 = GaussianM2VAE(conf2, name="m2")
 vae2.load(args.model_dir)
--- M1+M2/train.py
+++ M1+M2/train.py
@@ -14,7 +14,8 @@ if isinstance(vae1, GaussianM1VAE):
 dataset, labels = util.load_labeled_images(args.train_image_dir, dist=dist)
 
 max_epoch = 1000
-num_trains_per_epoch = 10000
+vae1_num_trains_per_epoch = 5000
+vae2_num_trains_per_epoch = 2000
 batchsize = 100
 
 # Create labeled/unlabeled split in training set
@@ -32,34 +33,32 @@ print "dataset:: labeled: {:d} unlabeled: {:d} validation: {:d}".format(len(labe
 total_time = 0
 for epoch in xrange(max_epoch):
 	# Train M1
-	if True:
-		sum_loss = 0
-		epoch_time = time.time()
-		for t in xrange(num_trains_per_epoch):
-			x = util.sample_x_variable(batchsize, conf1.ndim_x, dataset, gpu_enabled=conf1.gpu_enabled)
+	sum_loss = 0
+	epoch_time = time.time()
+	for t in xrange(vae1_num_trains_per_epoch):
+		x = util.sample_x_variable(batchsize, conf1.ndim_x, dataset, gpu_enabled=conf1.gpu_enabled)
 
-			# train
-			loss = vae1.train(x, L=1)
+		# train
+		loss = vae1.train(x, L=1)
 
-			sum_loss += loss
-			if t % 10 == 0:
-				sys.stdout.write("\rTraining M1 in progress...(%d / %d)" % (t, num_trains_per_epoch))
-				sys.stdout.flush()
-		epoch_time = time.time() - epoch_time
-		total_time += epoch_time
-		sys.stdout.write("\r")
-		print "[M1] epoch:", epoch, "loss: {:.3f}".format(sum_loss / num_trains_per_epoch), "time: {:d} min".format(int(epoch_time / 60)), "total: {:d} min".format(int(total_time / 60))
-		sys.stdout.flush()
-		vae1.save(args.model_dir)
+		sum_loss += loss
+		if t % 10 == 0:
+			sys.stdout.write("\rTraining M1 in progress...(%d / %d)" % (t, vae1_num_trains_per_epoch))
+			sys.stdout.flush()
+	epoch_time = time.time() - epoch_time
+	total_time += epoch_time
+	sys.stdout.write("\r")
+	print "[M1] epoch:", epoch, "loss: {:.3f}".format(sum_loss / vae1_num_trains_per_epoch), "time: {:d} min".format(int(epoch_time / 60)), "total: {:d} min".format(int(total_time / 60))
+	sys.stdout.flush()
+	vae1.save(args.model_dir)
 
-	continue
 
 	# Train M2
 	sum_loss_labeled = 0
 	sum_loss_unlabeled = 0
 	sum_loss_classifier = 0
 	epoch_time = time.time()
-	for t in xrange(num_trains_per_epoch):
+	for t in xrange(vae2_num_trains_per_epoch):
 		x_labeled, y_labeled, label_ids = util.sample_x_and_label_variables(batchsize, conf1.ndim_x, conf2.ndim_y, labeled_dataset, labels, gpu_enabled=conf2.gpu_enabled)
 		x_unlabeled = util.sample_x_variable(batchsize, conf1.ndim_x, unlabeled_dataset, gpu_enabled=conf2.gpu_enabled)
 		z_labeled = Variable(vae1.encoder(x_labeled, test=True, sample_output=True).data)
@@ -73,12 +72,12 @@ for epoch in xrange(max_epoch):
 		sum_loss_unlabeled += loss_unlabeled
 		sum_loss_classifier += loss_classifier
 		if t % 10 == 0:
-			sys.stdout.write("\rTraining M2 in progress...({:d} / {:d})".format(t, num_trains_per_epoch))
+			sys.stdout.write("\rTraining M2 in progress...({:d} / {:d})".format(t, vae2_num_trains_per_epoch))
 			sys.stdout.flush()
 	epoch_time = time.time() - epoch_time
 	total_time += epoch_time
 	sys.stdout.write("\r")
-	print "[M2] epoch:", epoch, "loss::", "labeled: {:.3f}".format(sum_loss_labeled / num_trains_per_epoch), "unlabeled: {:.3f}".format(sum_loss_unlabeled / num_trains_per_epoch), "classifier: {:.3f}".format(sum_loss_classifier / num_trains_per_epoch), "time: {:d} min".format(int(epoch_time / 60)), "total: {:d} min".format(int(total_time / 60))
+	print "[M2] epoch:", epoch, "loss::", "labeled: {:.3f}".format(sum_loss_labeled / vae2_num_trains_per_epoch), "unlabeled: {:.3f}".format(sum_loss_unlabeled / vae2_num_trains_per_epoch), "classifier: {:.3f}".format(sum_loss_classifier / vae2_num_trains_per_epoch), "time: {:d} min".format(int(epoch_time / 60)), "total: {:d} min".format(int(total_time / 60))
 	sys.stdout.flush()
 	vae2.save(args.model_dir)
 
